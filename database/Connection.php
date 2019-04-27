10167ad735113a0c52d294e43acbec20094d5a0e
Prakhar Agrawal <prakhargvp@gmail.com>
2019-04-27 11:44:54 +0530

    Hide Your Secret Passwords using config file

--- database/Connection.php
+++ database/Connection.php
@@ -2,9 +2,14 @@
 class Connection 
 {
 
-	public static function make() {
+	public static function make($config) {
 		try {
-			return new PDO('mysql:host=127.0.0.1;dbname=mytodo', 'prakhar', '');
+			return new PDO(
+				$config['connection'].';dbname='.$config['name'],
+				$config['username'],
+				$config['password'],
+				$config['options']
+			);
 		} catch(PDOException $e) {
 			die($e->getMessage());
 		}			
