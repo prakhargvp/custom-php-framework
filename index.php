8c343303eec67aaafd95aa822d8367822ec66af9
Prakhar Agrawal <prakhargvp@gmail.com>
2019-04-27 10:56:53 +0530

    Into to PDP 1.1

--- index.php
+++ index.php
@@ -1,17 +1,9 @@
 <?php 
+
 require 'functions.php';
+require 'Task.php';
 
-try {
-	$pdo = new PDO('mysql:host=127.0.0.1;dbname=mytodo', 'prakhar', '');
-} catch(PDOException $e) {
-	die($e->getMessage());
-}
-
-$statement = $pdo->prepare('select * from todos');
-
-$statement->execute();
-
-// Generally little careful about it, fetch
-$tasks = $statement->fetchAll(PDO::FETCH_OBJ);
+$pdo = connectToDb();
+$tasks = fetchAllTasks($pdo);
 
 require 'index.view.php';
