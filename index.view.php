f94dafbe6ee9405efc969d52b4622f0cab683b7c
Prakhar Agrawal <prakhargvp@gmail.com>
2019-04-29 07:30:11 +0530

    Adding about.php and contact.php file

--- index.view.php
+++ index.view.php
@@ -5,7 +5,12 @@
 	<title></title>
 </head>
 <body>
-	
+	<nav>
+		<ul>
+			<li><a href="/about.php">About Page</a></li>
+			<li><a href="/contact.php">Contact Page</a></li>
+		</ul>
+	</nav>
 	<ul>
 		<?php foreach ($tasks as $task) : ?>
 			<li>
